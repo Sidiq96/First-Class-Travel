c4ea9690ec401649fc1027bb3adcfd042eb2e9a1
Sidiq Hersi <hersisidiq@gmail.com>
2024-02-26 13:01:00 +0000

    adds destinations component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Destinations from "./components/Destinations";
 
 function App() {
   return <>
   <NavBar />
   <Hero />
+  <Destinations />
   </>;
 }
 
---
+++ src/components/Destinations.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import BoraBora from "../assets/images/borabora.jpg";
+import BoraBora2 from "../assets/images/borabora2.jpg";
+import Maldives from "../assets/images/maldives.jpg";
+import Maldives2 from "../assets/images/maldives2.jpg";
+import KeyWest from "../assets/images/keywest.jpg";
+
+const Destinations = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 text-center">
+      <h1>Top Destinations</h1>
+      <p className="py-4">Picking the right places</p>
+      <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4">
+        <img className="w-full h-full object-cover col-span-2 md:col-span-3 row-span-2" src={BoraBora} alt="" />
+        <img className="w-full h-full object-cover" src={BoraBora2} alt="" />
+        <img className="w-full h-full object-cover" src={Maldives} alt="" />
+        <img className="w-full h-full object-cover" src={Maldives2} alt="" />
+        <img className="w-full h-full object-cover" src={KeyWest} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Destinations;
