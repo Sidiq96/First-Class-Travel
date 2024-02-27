9d792adc84876e48796f517c591e2960e16179df
Sidiq Hersi <hersisidiq@gmail.com>
2024-02-27 13:32:22 +0000

    adds search component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Destinations from "./components/Destinations";
+import Search from "./components/Search";
 
 function App() {
   return <>
   <NavBar />
   <Hero />
   <Destinations />
+  <Search />
   </>;
 }
 
