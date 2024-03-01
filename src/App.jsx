21cd3dba0fbdf937437ac22c85998693f8b00e14
Sidiq Hersi <hersisidiq@gmail.com>
2024-03-01 11:45:17 +0000

    adds code for card component of the website

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Destinations from "./components/Destinations";
 import Search from "./components/Search";
+import Selects from "./components/Selects";
 
 function App() {
-  return <>
-  <NavBar />
-  <Hero />
-  <Destinations />
-  <Search />
-  </>;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Destinations />
+      <Search />
+      <Selects />
+    </>
+  );
 }
 
 export default App;
